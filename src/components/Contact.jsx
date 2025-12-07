1f85c6575272252c5ccf915242653c5ef6ca7af7
darbo-3 <darbomicr@gmail.com>
2025-12-07 14:18:07 +0300

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -52,12 +52,14 @@ const Contact = () => {
             }
 
             // Send email using EmailJS
-            const result = await emailjs.send(
-                process.env.EMAILJS_SERVICE_ID,
-                process.env.EMAILJS_TEMPLATE_ID,   
-                templateParams,
-                process.env.EMAILJS_PUBLIC_KEY   
-            )
+
+                const result = await emailjs.send(
+                    import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                    import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                    templateParams,
+                    import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+                ) 
+            
 
             setSuccessMessage('Sent')
             
